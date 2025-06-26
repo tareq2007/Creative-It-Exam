090dfc93a492dac3dcffd0c3465683ac95a1ff7f
unknown <tareqhasan2007@gmail.com>
2025-06-26 22:35:54 +0600

    Initial commit

--- user/models.py
+++ user/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Students(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     roll_number = models.IntegerField()
     departmet = models.CharField(max_length=50,blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
